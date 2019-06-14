9b8500871d7fd3bddeed353d176334197d784ed4
Sebastian Bittrich <bittrich@hs-mittweida.de>
2019-06-14 16:23:45 -0700

    adds authorship

--- src/apps/cif2bcif/converter.ts
+++ src/apps/cif2bcif/converter.ts
@@ -2,6 +2,7 @@
  * Copyright (c) 2017 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author David Sehnal <david.sehnal@gmail.com>
+ * @author Sebastian Bittrich <sebastian.bittrich@rcsb.org>
  */
 
 import { CIF, CifCategory, getCifFieldType, CifField, CifFile } from 'molstar/lib/mol-io/reader/cif'
